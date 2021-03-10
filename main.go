50241b6cfa64ab4be935d01294c05d9e4655828f
Nicholas Ulricksen <n.ulricksen@gmail.com>
2021-03-10 07:08:36 -0800

    add -t flag to load emulator test ROM

--- main.go
+++ main.go
@@ -1,23 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/n-ulricksen/chip8/core"
 )
 
 // The path to the ROM used to test our emulator.
-var rompath string = "./roms/CONNECT4"
+var (
+	rompath  string = "./roms/CONNECT4"
+	testpath string = "./test/BC_test.ch8"
+)
+
+var flagtest bool
+
+func init() {
+	flag.BoolVar(&flagtest, "t", false, "Load the emulator test ROM")
+	flag.Parse()
+}
 
 func main() {
 	chip8 := core.NewChip8()
 
-	fmt.Printf("Loading ROM from %s\n", rompath)
-	chip8.LoadRom(rompath)
+	if flagtest {
+		fmt.Printf("Loading test ROM from %s\n", testpath)
+		chip8.LoadRom(testpath)
+	} else {
+		fmt.Printf("Loading ROM from %s\n", rompath)
+		chip8.LoadRom(rompath)
+	}
 
 	fmt.Println("Starting program...")
 	fmt.Println()
 
 	chip8.Run()
-
 }
